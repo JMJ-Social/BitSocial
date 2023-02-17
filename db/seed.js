0a4a938a2115bcedbecb3b73efe312ecf479c00a
cusmoritz <cus.moritz@gmail.com>
2023-02-16 18:19:15 -0700

    feat(seed): Created 5 database seed users

---
+++ db/seed.js
@@ -0,0 +1,46 @@
+// import client from /index
+const client = require('/index');
+
+// seed data for db initialization
+
+export const fakeUsers = [
+    {   firstName: "John",
+        lastName: "Jingleheimer",
+        username: "jingleHeimer",
+        password: "J0hnJ1ngle",
+        email: "johnJ@gmail.com",
+        phone: 9709709700,
+    },
+    {
+        firstName: "Sandra",
+        lastName: "Herowitz",
+        username: "sandyS",
+        password: "P4ssw0rd!",
+        email: "sandra@hotmail.net",
+        phone: 8795439870,
+    },
+    {
+        firstName: "James",
+        lastName: "",
+        username: "",
+        password: "",
+        email: "",
+        phone: ,
+    },
+    {
+        firstName: "Ian",
+        lastName: "Hamilton",
+        username: "HamMan",
+        password: "ians.passw0rd",
+        email: "ianHere@netscape.gov",
+        phone: 5824687946,
+    },
+    {
+        firstName: "Allison",
+        lastName: "bitSocial",
+        username: "4llison",
+        password: "4llisonBitS0cial.",
+        email: "this4llison",
+        phone: 3219875698,
+    },
+]
